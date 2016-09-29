9229ef4cfeb0d52b9074d452547493ab6285989e
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2016-09-29 19:04:10 +0200

    set default value for refresh rate

--- app/src/main/java/io/github/sekhmet/dash/SourceSettingsFragment.java
+++ app/src/main/java/io/github/sekhmet/dash/SourceSettingsFragment.java
@@ -39,7 +39,7 @@ public class SourceSettingsFragment extends PreferenceFragment {
         listPreference.setSummary(getString(R.string.pref_source_refresh_frequency_summary));
         listPreference.setEntries(R.array.pref_source_refresh_frequency_entries);
         listPreference.setEntryValues(refreshRateKeys);
-        listPreference.setValueIndex(3);
+        listPreference.setDefaultValue(2);
         screen.addItemFromInflater(listPreference);
     }
 }
